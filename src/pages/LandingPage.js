10afcd02c244b6faa69b2124fc9b917185247551
KurniaMulyaR <63498139+KurniaMulyaR@users.noreply.github.com>
2020-05-06 20:22:36 +0700

    Staycation
    Web perhotelan

---
+++ src/pages/LandingPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import ModuleName from 'elements/button'
+
+export default class LandingPage extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
