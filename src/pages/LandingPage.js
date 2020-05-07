2f94663dd398e5b162701d4f6aaf98d4bba0e28c
KurniaMulyaR <63498139+KurniaMulyaR@users.noreply.github.com>
2020-05-07 23:29:46 +0700

    update bagian header dan hero

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 
 import Header from 'parts/Header';
+import landingPage from 'json/landingPage.json';
+import Hero from 'parts/Hero';
 
 
 export default class LandingPage extends Component {
@@ -10,6 +12,7 @@ export default class LandingPage extends Component {
             
             <>
                  <Header {...this.props}></Header>
+                 <Hero data={landingPage.hero}/>
             </>
         );
     }
