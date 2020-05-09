b5ecfd2b46902c1b677df48f06fc21f1bc6d2a84
KurniaMulyaR <63498139+KurniaMulyaR@users.noreply.github.com>
2020-05-09 16:07:17 +0700

    fix misstype component parts/footes

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -5,6 +5,8 @@ import landingPage from 'json/landingPage.json';
 import Hero from 'parts/Hero';
 import MostPicked from 'parts/MostPicked';
 import Categories from 'parts/Categories';
+import Testimoni from 'parts/Testimoni';
+import Footer from 'parts/Footer';
 
 
 export default class LandingPage extends Component {
@@ -20,6 +22,8 @@ export default class LandingPage extends Component {
                  <Hero refMostPicked={this.refMostPicked} data={landingPage.hero}/>
                  <MostPicked refMostPicked={this.refMostPicked} data={landingPage.mostPicked}/>
                  <Categories data={landingPage.categories}/>
+                 <Testimoni data={landingPage.testimonial} />
+                 <Footer />
             </>
         );
     }
